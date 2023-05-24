4ca34de469d06814764c44a407cb12cb60bc7595
ShuangXu-project <sxuu0044@gmail.com>
2023-05-24 09:07:36 +0800

    make add to cart button interactiv3e

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -56,3 +56,26 @@ products.forEach((product) => {
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
+document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
+  button.addEventListener('click',() =>{
+    const productId= button.dataset.productid;
+    let matchingItem;
+    cart.forEach((item)=>{
+      if(productId ===item.productId){
+        matchingItem=item;
+      }
+    });
+    if(matchingItem){
+      matchingItem.quantity +=1;
+    }else{
+      cart.push({
+        productId:productId,
+        quantity:1
+      });
+
+    }
+    console.log(cart);
+   
+  });
+
+});
